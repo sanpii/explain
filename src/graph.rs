817def8ccbf24b18b21504a9bc56972790122054
Sanpi <sanpi@homecomputing.fr>
2020-03-21 21:40:08 +0100

    Moves functions to impl

--- src/graph.rs
+++ src/graph.rs
@@ -10,6 +10,7 @@ struct Graph {
     edges: Vec<(usize, usize)>,
     current_id: usize,
     max_cost: f32,
+    execution_time: f32,
 }
 
 impl Graph {
@@ -19,12 +20,17 @@ impl Graph {
             edges: Vec::new(),
             current_id: 0,
             max_cost: 0.,
+            execution_time: 0.,
         }
     }
 
     fn from(explain: &crate::Explain) -> Self {
         let mut graph = Self::new();
 
+        graph.execution_time = explain
+            .execution_time
+            .or(explain.plan.actual_total_time)
+            .unwrap();
         graph.plan(&explain, None, &explain.plan);
 
         graph
@@ -34,7 +40,7 @@ impl Graph {
         let id = self.current_id;
         self.current_id += 1;
 
-        let node = Node::from(explain, plan);
+        let node = Node::from(plan);
         if node.cost > self.max_cost {
             self.max_cost = node.cost;
         }
@@ -60,50 +66,139 @@ impl Graph {
     fn node(&self, n: Nd) -> Option<&Node> {
         self.nodes.get(n)
     }
+
+    fn duration_color(percent: f32) -> &'static str {
+        if percent > 90. {
+            "#880000"
+        } else if percent > 40. {
+            "#ee8800"
+        } else if percent > 10. {
+            "#fddb61"
+        } else {
+            "white"
+        }
+    }
+
+    fn color(percent: f32) -> String {
+        let hue = (100. - percent * 100.) * 1.2 / 360.;
+        let (red, green, blue) = Self::hsl_to_rgb(hue, 0.9, 0.4);
+
+        format!("#{:02x}{:02x}{:02x}", red, green, blue)
+    }
+
+    fn hsl_to_rgb(hue: f32, saturation: f32, lightness: f32) -> (u8, u8, u8) {
+        let (red, green, blue) = if saturation != 0. {
+            let q = if lightness < 0.5 {
+                lightness * (1. + saturation)
+            } else {
+                lightness + saturation - lightness * saturation
+            };
+            let p = 2. * lightness - q;
+
+            (
+                Self::hue2rgb(p, q, hue + 1. / 3.),
+                Self::hue2rgb(p, q, hue),
+                Self::hue2rgb(p, q, hue - 1. / 3.),
+            )
+        } else {
+            (lightness, lightness, lightness)
+        };
+
+        (
+            (red * 255.) as u8,
+            (green * 255.) as u8,
+            (blue * 255.) as u8,
+        )
+    }
+
+    fn hue2rgb(p: f32, q: f32, t: f32) -> f32 {
+        let mut t = t;
+
+        if t < 0. {
+            t += 1.;
+        }
+        if t > 1. {
+            t -= 1.;
+        }
+        if t < 1. / 6. {
+            return p + (q - p) * 6. * t;
+        }
+        if t < 1. / 2. {
+            return q;
+        }
+        if t < 2. / 3. {
+            return p + (q - p) * (2. / 3. - t) * 6.;
+        }
+
+        p
+    }
 }
 
 struct Node {
     n_workers: usize,
     info: String,
     rows: u32,
-    time: String,
+    time: Option<f32>,
     cost: f32,
     ty: String,
 }
 
 impl Node {
-    fn from(explain: &crate::Explain, plan: &crate::Plan) -> Self {
-        let info = info(&plan);
-        let cost = cost(&plan);
-        let time = if let Some(time) = time(&plan) {
-            let execution_time = explain
-                .execution_time
-                .or(explain.plan.actual_total_time)
-                .unwrap();
-            let time_percent = (time / execution_time * 100.).round().trunc();
-
-            if time < 1. {
-                format!("<td>&lt; 1 ms | {} %</td>", time_percent)
-            } else {
-                let color = duration_color(time_percent);
-
-                format!(
-                    "<td bgcolor=\"{}\">{:.2} ms | {} %</td>",
-                    color, time, time_percent
-                )
-            }
-        } else {
-            String::new()
-        };
-
+    fn from(plan: &crate::Plan) -> Self {
         Self {
-            info,
+            info: Self::info(&plan),
             n_workers: plan.workers.len(),
             rows: plan.rows,
-            time,
+            time: Self::time(&plan),
             ty: plan.node.to_string(),
-            cost,
+            cost: Self::cost(&plan),
         }
+    }
+
+    fn info(plan: &crate::Plan) -> String {
+        match &plan.node {
+            crate::Node::Aggregate { keys, .. } => {
+                if keys.is_empty() {
+                    String::new()
+                } else {
+                    format!("by {}", keys.join(", "))
+                }
+            }
+            crate::Node::HashJoin {
+                join_type,
+                hash_cond,
+                ..
+            } => format!("{} join on {}", join_type, hash_cond),
+            crate::Node::Sort { keys, .. } => format!("by {}", keys.join(", ")),
+            crate::Node::SeqScan { relation, .. } => format!("on {}", relation),
+            _ => String::new(),
+        }
+    }
+
+    fn time(plan: &crate::Plan) -> Option<f32> {
+        if let Some(mut time) = plan.actual_total_time {
+            time *= plan.actual_loops as f32;
+
+            for child in &plan.plans {
+                if child.parent_relationship != Some("InitPlan".to_string()) {
+                    time -= child.actual_total_time.unwrap_or_default() * child.actual_loops as f32;
+                }
+            }
+
+            Some(time)
+        } else {
+            None
+        }
+    }
+
+    fn cost(plan: &crate::Plan) -> f32 {
+        let mut cost = plan.total_cost;
+
+        for child in &plan.plans {
+            cost -= child.total_cost;
+        }
+
+        cost.max(0.)
     }
 }
 
@@ -119,7 +214,8 @@ impl<'a> dot::Labeller<'a, Nd, Ed<'a>> for Graph {
     fn node_label<'b>(&'b self, n: &Nd) -> dot::LabelText<'b> {
         let node = self.node(*n).unwrap();
         let percent = node.cost / self.max_cost;
-        let color = color(percent);
+        let color = Self::color(percent);
+
         let bgcolor = if percent < 0.1 {
             String::new()
         } else if percent > 0.99 {
@@ -128,10 +224,27 @@ impl<'a> dot::Labeller<'a, Nd, Ed<'a>> for Graph {
             format!("bgcolor=\"{};{:.2}:white\"", color, percent)
         };
 
+        let time = if let Some(time) = node.time {
+            let time_percent = (time / self.execution_time * 100.).round().trunc();
+
+            if time < 1. {
+                format!("<td>&lt; 1 ms | {} %</td>", time_percent)
+            } else {
+                let color = Self::duration_color(time_percent);
+
+                format!(
+                    "<td bgcolor=\"{}\">{:.2} ms | {} %</td>",
+                    color, time, time_percent
+                )
+            }
+        } else {
+            String::new()
+        };
+
         let mut label = r#"<table border="0" cellborder="0" cellspacing="5">"#.to_string();
         label.push_str(&format!(
             r#"<tr><td align="left"><b>{}</b></td>{}</tr>"#,
-            node.ty, node.time
+            node.ty, time
         ));
         label.push_str(&format!(
             r#"<tr><td colspan="2" align="left">{}</td></tr>"#,
@@ -193,116 +306,4 @@ impl<'a> dot::GraphWalk<'a, Nd, Ed<'a>> for Graph {
     fn target(&self, e: &Ed<'_>) -> Nd {
         e.1
     }
-}
-
-fn duration_color(percent: f32) -> &'static str {
-    if percent > 90. {
-        "#880000"
-    } else if percent > 40. {
-        "#ee8800"
-    } else if percent > 10. {
-        "#fddb61"
-    } else {
-        "white"
-    }
-}
-
-fn color(percent: f32) -> String {
-    let hue = (100. - percent * 100.) * 1.2 / 360.;
-    let (red, green, blue) = hsl_to_rgb(hue, 0.9, 0.4);
-
-    format!("#{:02x}{:02x}{:02x}", red, green, blue)
-}
-
-fn hsl_to_rgb(hue: f32, saturation: f32, lightness: f32) -> (u8, u8, u8) {
-    let (red, green, blue) = if saturation != 0. {
-        let q = if lightness < 0.5 {
-            lightness * (1. + saturation)
-        } else {
-            lightness + saturation - lightness * saturation
-        };
-        let p = 2. * lightness - q;
-
-        (
-            hue2rgb(p, q, hue + 1. / 3.),
-            hue2rgb(p, q, hue),
-            hue2rgb(p, q, hue - 1. / 3.),
-        )
-    } else {
-        (lightness, lightness, lightness)
-    };
-
-    (
-        (red * 255.) as u8,
-        (green * 255.) as u8,
-        (blue * 255.) as u8,
-    )
-}
-
-fn hue2rgb(p: f32, q: f32, t: f32) -> f32 {
-    let mut t = t;
-
-    if t < 0. {
-        t += 1.;
-    }
-    if t > 1. {
-        t -= 1.;
-    }
-    if t < 1. / 6. {
-        return p + (q - p) * 6. * t;
-    }
-    if t < 1. / 2. {
-        return q;
-    }
-    if t < 2. / 3. {
-        return p + (q - p) * (2. / 3. - t) * 6.;
-    }
-
-    p
-}
-
-fn info(plan: &crate::Plan) -> String {
-    match &plan.node {
-        crate::Node::Aggregate { keys, .. } => {
-            if keys.is_empty() {
-                String::new()
-            } else {
-                format!("by {}", keys.join(", "))
-            }
-        }
-        crate::Node::HashJoin {
-            join_type,
-            hash_cond,
-            ..
-        } => format!("{} join on {}", join_type, hash_cond),
-        crate::Node::Sort { keys, .. } => format!("by {}", keys.join(", ")),
-        crate::Node::SeqScan { relation, .. } => format!("on {}", relation),
-        _ => String::new(),
-    }
-}
-
-fn time(plan: &crate::Plan) -> Option<f32> {
-    if let Some(mut time) = plan.actual_total_time {
-        time *= plan.actual_loops as f32;
-
-        for child in &plan.plans {
-            if child.parent_relationship != Some("InitPlan".to_string()) {
-                time -= child.actual_total_time.unwrap_or_default() * child.actual_loops as f32;
-            }
-        }
-
-        Some(time)
-    } else {
-        None
-    }
-}
-
-fn cost(plan: &crate::Plan) -> f32 {
-    let mut cost = plan.total_cost;
-
-    for child in &plan.plans {
-        cost -= child.total_cost;
-    }
-
-    cost.max(0.)
 }
