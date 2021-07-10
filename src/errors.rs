fa370be78bcac6e2b0cf645aa4140593523cdc91
Sanpi <sanpi@homecomputing.fr>
2021-07-10 14:12:52 +0200

    Uses thiserror crate

--- src/errors.rs
+++ src/errors.rs
@@ -1,41 +1,13 @@
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    Convert(std::num::ParseIntError),
-    Io(std::io::Error),
-    Elephantry(elephantry::Error),
-    Serde(serde_json::Error),
-}
-
-impl std::error::Error for Error {}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl From<elephantry::Error> for Error {
-    fn from(error: elephantry::Error) -> Self {
-        Self::Elephantry(error)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(error: serde_json::Error) -> Self {
-        Self::Serde(error)
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(error: std::io::Error) -> Self {
-        Self::Io(error)
-    }
-}
-
-impl From<std::num::ParseIntError> for Error {
-    fn from(error: std::num::ParseIntError) -> Self {
-        Self::Convert(error)
-    }
+    #[error("{0}")]
+    Convert(#[from] std::num::ParseIntError),
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+    #[error("{0}")]
+    Elephantry(#[from] elephantry::Error),
+    #[error("{0}")]
+    Serde(#[from] serde_json::Error),
 }
